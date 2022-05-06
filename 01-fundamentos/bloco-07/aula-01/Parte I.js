6f9b1e0a56842fe538d3ab86e4630f8fbff79c41
Joao Sousa <joaovitors@outlook.com>
2022-05-06 19:36:35 -0300

    Parte I dos exercícios do dia

---
+++ 01-fundamentos/bloco-07/aula-01/Parte I.js
@@ -0,0 +1,19 @@
+// function testingScope(escopo) {
+//   if (escopo === true) {
+//     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//     console.log(ifScope);
+//   } else {
+//     var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+//   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+// }
+
+// testingScope(true);
+
+const testingScope = (escopo) => ((escopo) ? `Não devo ser utilizada fora do meu escopo (if) ${' ótimo, fui utilizada no escopo !'}`
+ : `Não devo ser utilizada fora meu escopo (else)`);
+
+console.log(testingScope(true));
+console.log(testingScope(false));
