debd8e2bd43e903dd7d0d2ab42bc1ef83ce68d2b
Joao Sousa <joaovitors@outlook.com>
2022-05-13 16:17:21 -0300

    Para fixar

---
+++ 01-fundamentos/bloco-08/aula-01/para-fixar.js
@@ -0,0 +1,9 @@
+const wakeUp = () => 'Acordando!!';
+const breakfast = () => 'Bora tomar café!!';
+const sleep = () => 'Partiu dormir!!';
+
+const doingThings = (func) => console.log(func);
+
+doingThings(wakeUp());
+doingThings(breakfast());
+doingThings(sleep());
