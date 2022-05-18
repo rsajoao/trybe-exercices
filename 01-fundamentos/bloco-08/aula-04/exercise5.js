5e97b4f16f1c632af7326e3421880aff74e9be26
Joao Sousa <joaovitors@outlook.com>
2022-05-18 19:59:00 -0300

    Exercício 5

---
+++ 01-fundamentos/bloco-08/aula-04/exercise5.js
@@ -0,0 +1,19 @@
+// 5. Dada o array de nomes, retorne a quantidade de vezes que aparecem a letra a maiúscula ou minúscula
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+const contemA = (array) => {
+  return array.reduce((A, palavra) => {
+    return A += palavra.split('').reduce((a, letra) => {
+      if (letra === 'a' || letra === 'A') {
+        return a += 1;
+      }
+      return a;
+    },0)
+  },0)
+}
+console.log(contemA(names))
